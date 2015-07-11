b8164a1e5084bdc8f8d2a84ddf5a0a601a29de7c
tiger <tigerskitchen@users.noreply.github.com>
2015-07-10 22:59:31 -0500

    Clarity

--- howmanyin.py
+++ howmanyin.py
@@ -1,31 +1,32 @@
+#!/usr/bin/python3
+#This scraper is specific to IMDB's URLs
+#Typical usage:
+#python howmanyin.py url namevariation1 namevariation2 etc...
+
 from bs4 import BeautifulSoup
 import requests
 from sys import argv
 import re
 
-#This scraper is specific to IMDB's URLs
-#Typical usage:
-#python howmanyin.py url namevariation1 namevariation2 etc...
-
-#Kung Fu Panda is the default url if none is provided
-
-kung_fu = 'http://www.imdb.com/title/tt0441773/fullcredits'
+default_movie = 'http://www.imdb.com/title/tt0441773/fullcredits'
+#Default url if none is provided
 
 if len(argv) == 1:
-    url = kung_fu 
+    url = default_movie 
 elif argv[1].startswith("http://www.imdb.com/"):
     movie_id = re.sub('\D', '', argv[1])
     url = 'http://www.imdb.com/title/tt' + movie_id[:7] + 'fullcredits'
 else:
-    argv.insert(1, kung_fu) 
-    url = kung_fu
+    argv.insert(1, default_movie) 
+    url = default_movie
 
-#Additional names can be added to the command line arguments after the the addition of a url
 
 if len(argv) < 3:
     names_to_search = ('Matt', 'Matthew', 'Matti', 'Matty', 'Mat', 'Mathew')
+    #Default names to search if none is provided
 else:
     names_to_search = tuple([str(i.lower().capitalize()) for i in argv[2:]])
+    #Add names as command line arguments to override default names to search
 
 r = requests.get(url)
 soup = BeautifulSoup(r.text)
@@ -34,8 +35,8 @@ links = soup.find_all('a')
 movie_title = soup.find('a', class_='subnav_heading').string
 
 all_names = [i.string.strip() for i in links if i.string != None]
-final_results = [i for i in all_names if i.startswith(names_to_search)]
+filtered_names = [i for i in all_names if i.startswith(names_to_search)]
 
 if __name__ == '__main__':
-    print(str(movie_title) + " has " + str(len(final_results)) + "!!!")
-    print(final_results)
+    print(str(movie_title) + " has " + str(len(filtered_names)) + "!!!")
+    print(filtered_names)
